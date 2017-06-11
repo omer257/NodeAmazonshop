ced5a4cadc460f2083d133b59964e60dacb5213e
omer257 <omer257@gmail.com>
2017-06-11 15:45:54 +0300

    Changed json dependecy to fake node api service

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+var amazon = require('amazon-product-api');
+const app = express()
+
+
+var client = amazon.createClient({
+    awsId: "AKIAJ6EFCIATZ37HS7IA",
+    awsSecret: "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",
+    awsTag: "aws Tag"
+});
+
+app.use(express.static('./'));
+
+app.get('/', function (req, res) {
+    client.itemSearch({
+        director: 'Quentin Tarantino',
+    }).then(function (results) {
+        res.send(results)
+    }).catch(function (err) {
+        res.send(err)
+    });
+})
+
+var fakeJson = [
+    {
+        img: 'http://ghk.h-cdn.co/assets/cm/15/11/550003f16c562-clotheshanger-lgn.jpg',
+        name: 'fake prd 1',
+        price: 22
+    },{
+        img: 'http://ghk.h-cdn.co/assets/cm/15/11/550003f16c562-clotheshanger-lgn.jpg',
+        name: 'fake prd 21',
+        price: 55
+    },{
+        img: 'http://ghk.h-cdn.co/assets/cm/15/11/550003f16c562-clotheshanger-lgn.jpg',
+        name: 'fake prd 41',
+        price: 85
+    },{
+        img: 'http://www.dumpaday.com/wp-content/uploads/2012/02/51YSPKzdyLL._SL500_AA300_.jpg',
+        name: 'fake prd 51',
+        price: 456
+    },{
+        img: 'http://ghk.h-cdn.co/assets/cm/15/11/550003f16c562-clotheshanger-lgn.jpg',
+        name: 'fake prd 17',
+        price: 88
+    },{
+        img: 'http://ghk.h-cdn.co/assets/cm/15/11/550003f16c562-clotheshanger-lgn.jpg',
+        name: 'fake prd 81',
+        price: 99
+    },{
+        img: 'http://www.dumpaday.com/wp-content/uploads/2012/02/51YSPKzdyLL._SL500_AA300_.jpg',
+        name: 'fake prd 19',
+        price: 12
+    },
+]
+
+app.get('/json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(fakeJson, null, 3));
+})
+
+app.get('/index', function (req, res) {
+    var cUrl = req.query;
+    res.send('Hello index!' + cUrl)
+})
+
+app.get('/me', function (req, res) {
+    res.send(me.myobj.name)
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 // an array with all of our cart items
+var $ = $;
 var cart = [];
 var source = $("#entry-template").html();
 var template = Handlebars.compile(source);
+
+
+var sourcePrd = $("#product-template").html();
+var templatePrd = Handlebars.compile(sourcePrd);
 var updateCart = function () {
-    // TODO: Write this function. In this function we render the page.
-    // Meaning we make sure that all our cart items are displayed in the browser.
-    // Remember to empty the "cart div" before you re-add all the item elements.
-    //    console.log(cart);
     $('.cart-list').empty();
-    var total = 0;
+    let total = 0;
     cart.forEach((item, val) => {
         $('.cart-list').append(template({
             amount: cart[val].amount,
             name: cart[val].name,
             price: cart[val].price
         }));
-        total = total + parseInt(cart[val].price)* parseInt(cart[val].amount);
+        total = total + parseInt(cart[val].price) * parseInt(cart[val].amount);
     });
     $('.total').html(total);
 }
 var addItem = function (item) {
-    // TODO: Write this function. Remember this function has nothing to do with display. 
-    // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-    var foundIndex= cart.findIndex((itemX) => {
+    var foundIndex = cart.findIndex((itemX) => {
         return itemX.name === item.name;
     });
-    if(foundIndex===-1){
+    if (foundIndex === -1) {
         cart.push(item);
-    }
-    else{
-        cart[foundIndex].amount +=1 ;
+    } else {
+        cart[foundIndex].amount += 1;
     }
 }
 var clearCart = function () {
@@ -37,20 +35,32 @@ var clearCart = function () {
     updateCart();
     // TODO: Write a function that clears the cart ;-)
 }
+var buildProducts = function () {
+    var jqxhr = $.getJSON("/json", function (data) {
+            data.forEach((item) => {
+                $('.products').append(templatePrd({
+                    img: item.img,
+                    name: item.name,
+                    price: item.price
+                }));
+            })
+
+        })
+        .fail(function () {
+            console.log("error");
+        })
+}
 $('.view-cart').on('click', function () {
-    // TODO: hide/show the shopping cart!
     $('.shopping-cart').toggle();
 });
 
 
-$('body').on('click', '.remove', function() {
-    // do something
-    cart.splice($(this).index(),1);
+$('body').on('click', '.remove', function () {
+    cart.splice($(this).index(), 1);
     updateCart();
 });
- 
-$('.add-to-cart').on('click', function () {
-    // TODO: get the "item" object from the page
+
+$('body').on('click', '.add-to-cart', function () {
     var minElem = $(this).parents('.item');
     var p_price = minElem.attr("data-price");
     var p_name = minElem.attr("data-name");
@@ -66,3 +76,4 @@ $('.clear-cart').on('click', function () {
 });
 // update the cart as soon as the page loads!
 updateCart();
+buildProducts();
